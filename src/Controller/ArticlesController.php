aed902384df09e8094a762a42cc0199fcead5d35
budougumi0617 <budougumi0617@gmail.com>
2019-10-10 18:46:24 +0900

    :sparkles: add feature to delete content

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -60,4 +60,16 @@ class ArticlesController extends AppController
 
         $this->set('article', $article);
     }
+
+    // https://book.cakephp.org/3.0/ja/tutorials-and-examples/cms/articles-controller.html#delete
+    public function delete($slug)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        if ($this->Articles->delete($article)) {
+            $this->Flash->success(__('The {0} article has been deleted.', $article->title));
+            return $this->redirect(['action' => 'index']);
+        }
+    }
 }
